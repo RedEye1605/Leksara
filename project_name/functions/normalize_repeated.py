946b0485d4ea48bc7fee30a0e845bcf1024064e4
Althaf Rizal <ilikemud7@gmail.com>
2025-09-04 13:41:33 +0700

    feat: normalize character

--- project_name/functions/normalize_repeated.py
+++ project_name/functions/normalize_repeated.py
@@ -7,4 +7,19 @@ def normalize_repeated(text: str, max_repeat: int = 2) -> str:
     
     TODO: Implementasi fungsi ini oleh kontributor selanjutnya.
     """
-    raise NotImplementedError("normalize_repeated belum diimplementasikan.")
+    if max_repeat < 1:
+        raise ValueError("max_repeat must be >= 1")
+
+    # Regex: (.)\1{n,} menangkap karakter yang diulang lebih dari n kali
+    # Hanya normalisasi jika pengulangan >= max_repeat+1
+    if max_repeat == 1:
+        pattern = re.compile(r"(.)\1{" + str(max_repeat+1) + r",}")
+    else: 
+        pattern = re.compile(r"(.)\1{" + str(max_repeat) + r",}")
+    return pattern.sub(lambda m: m.group(1) * max_repeat, text)
+
+if __name__ == "__main__":
+    print(normalize_repeated("hello everrryoonneeeee", max_repeat=1))
+    # 👉 "pekerjaan mereka itu jelek sekali"  ✅
+    
+    print(normalize_repeated("hello everrryoonneeeee", max_repeat=2))
