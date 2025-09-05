70f99d4a7ac9e3ae2b3edfe6f59d1136ae965c51
Althaf Rizal <77833189+TrashAlpha@users.noreply.github.com>
2025-09-05 17:29:45 +0700

    feat: normalize character refs #22
    * feat: normalize character
    
    * fix: penyesuaian untuk normalisasi kata indonesia
    
    * test: add heuristic and unit test
    
    * test: add some normal indonesian word test
    
    * refactor: delete test

---
+++ tests/test_normalize_repeated.py
@@ -0,0 +1,60 @@
+import unittest
+from project_name.functions.normalize_repeated import normalize_repeated
+
+class TestNormalizeRepeated(unittest.TestCase):
+    def test_max_repeat_validation(self):
+        with self.assertRaises(ValueError):
+            normalize_repeated("aaaa", max_repeat=0)
+
+    def test_end_word_double_char_removed(self):
+        # "pekerjaann" -> "pekerjaan"
+        self.assertEqual(normalize_repeated("pekerjaann", max_repeat=1), "pekerjaan")
+
+    def test_end_word_many_char_removed(self):
+        # "sekaliiii" -> "sekali"
+        self.assertEqual(normalize_repeated("sekaliiii", max_repeat=1), "sekali")
+
+    def test_middle_word_repeated(self):
+        # "jeeelek" -> "jelek"
+        self.assertEqual(normalize_repeated("jeeelek", max_repeat=1), "jelek")
+
+    def test_middle_word_repeated_2(self):
+        # "baguuuuus" -> "bagus"
+        self.assertEqual(normalize_repeated("baguuuuus", max_repeat=1), "bagus")
+
+    def test_combo_word(self):
+        # "mantaaappp" -> "mantap"
+        self.assertEqual(normalize_repeated("mantaaappp", max_repeat=1), "mantap")
+
+    def test_normal_indonesian_word(self):
+        # "tinggiiiii" -> "tinggi"
+        self.assertEqual(normalize_repeated("tinggiiiii", max_repeat=1), "tinggi")
+    
+    def test_normal_indonesian_word_2(self):
+        # "assalamualaikumm" -> "assalamualaikum"
+        self.assertEqual(normalize_repeated("assalamualaikumm", max_repeat=1), "assalamualaikum")
+    
+    def test_normal_indonesian_word_3(self):
+        # "maaf" -> "maaf"
+        self.assertEqual(normalize_repeated("maaf", max_repeat=1), "maaf")
+
+    def test_max_repeat_2_behavior(self):
+        # "mantaaapppp" -> "mantaapp"
+        self.assertEqual(normalize_repeated("mantaaapppp", max_repeat=2), "mantaapp")
+
+    def test_max_repeat_2_huruf(self):
+        # "hiiiii" -> "hii"
+        self.assertEqual(normalize_repeated("hiiiii", max_repeat=2), "hii")
+
+    def test_sentence_case(self):
+        txt = "peeeekerjaann mereka itu jeeelllleeeeekkk sekaliiiiiiiiii"
+        out = normalize_repeated(txt, max_repeat=1)
+        # Pastikan kata "peeeekerjaann" jadi "pekerjaan"
+        self.assertIn("pekerjaan", out)
+        # Pastikan kata akhir "sekaliiiiiiiiii" jadi "sekali"
+        self.assertIn("sekali", out)
+        # Tidak boleh ada sisa "iiii"
+        self.assertNotIn("iiii", out)
+
+if __name__ == '__main__':
+    unittest.main()
