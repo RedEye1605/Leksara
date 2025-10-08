0ce8a916faf7328a7f22d9f5ec5075c59868a5a3
Ditt-A <aditya.akkbbar@gmail.com>
2025-10-09 03:35:32 +0700

    Fix:PII function and regex

--- leksara/tests/test_patterns_pii.py
+++ leksara/tests/test_patterns_pii.py
@@ -1,2 +1,13 @@
+from leksara.functions.patterns.pii import (
+    replace_phone,
+    replace_address,
+    replace_email,
+    replace_id
+)
+
+
+def test_replace_phone():
+    text
+
 def test_patterns_pii():
     pass
